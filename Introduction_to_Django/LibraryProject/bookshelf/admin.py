bd44a088f6d1c853cd685a5ebc49e7c4df3f4750
andreakbennett <andreakbennett365@gmail.com>
2024-11-10 21:24:55 +0200

    Add files via upload

---
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
