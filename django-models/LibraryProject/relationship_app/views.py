ae41ca3e1474d8a05fc2d41c1c07063e0f511833
andreakbennett <andreakbennett365@gmail.com>
2024-11-17 09:45:50 +0200

    Add files via upload

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,80 @@
+# relationship_app/views.py
+from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+from django.views.generic.detail import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+
+# Login View
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to home or another page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+# Logout View
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+# Registration View
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to home or another page
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, HttpResponse
+
+# Check user role
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin View
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("<h1>Welcome Admin</h1>")
+
+# Librarian View
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return HttpResponse("<h1>Welcome Librarian</h1>")
+
+# Member View
+@user_passes_test(is_member)
+def member_view(request):
+    return HttpResponse("<h1>Welcome Member</h1>")
+
