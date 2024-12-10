f8392db9f845d417333c89234ba4a8a2c5967208
andreakbennett <andreakbennett365@gmail.com>
2024-12-10 21:07:12 +0200

    Add files via upload

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+
+
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from .models import CustomUser
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        User = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        Token.objects.create(user=User)  # Create token for the new user
+        return User
+
---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import RegisterView, ProfileView
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('login/', obtain_auth_token, name='login') ,
+]
+
