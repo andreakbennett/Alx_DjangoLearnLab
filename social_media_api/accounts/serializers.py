af22fe693b8e73e21f27e4dfb7fcfa49ad9ab591
andreakbennett <andreakbennett365@gmail.com>
2024-12-10 20:38:04 +0200

    Add files via upload

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+
+
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        Token.objects.create(user=user)  # Create token for the new user
+        return user
